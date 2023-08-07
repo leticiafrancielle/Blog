58322b545aa160a08b1431d14cd7b1b9f972bc24
leticiafrancielle <leticiafrancielle2005@gmail.com>
2023-08-07 00:05:45 -0300

    criando alerts

--- src/pages/perfil/Perfil.tsx
+++ src/pages/perfil/Perfil.tsx
@@ -1,7 +1,7 @@
 import React, { useContext, useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { AuthContext } from '../../contexts/AuthContext'
-import loginLogo from '../../assets/login.jpg'
+import loginLogo from '../../assets/fundo.jpg'
 import { toastAlerta } from '../../utils/toastAlerta'
 function Perfil() {
   let navigate = useNavigate()
